4e193b0f2a5f8bb9493fe6bcee72d4e708dda682
thompsonjonm <thompson.jonm@gmail.com>
2021-01-18 10:36:47 -0500

    Add prettier

--- cypress/integration/demoqa/elements.spec.js
+++ cypress/integration/demoqa/elements.spec.js
@@ -1,54 +1,43 @@
 describe('DemoQA Elements Page', () => {
-    specify('As a user, I should be able to visit the Elements page', () => {
-        cy.visit(`${Cypress.config('baseUrl')}/elements`);
+  specify('As a user, I should be able to visit the Elements page', () => {
+    cy.visit(`${Cypress.config('baseUrl')}/elements`);
 
-        cy.get('.header-text')
-          .invoke('text')
-          .should('contain', 'Elements');
+    cy.get('.header-text').invoke('text').should('contain', 'Elements');
+  });
+
+  specify('As a user, I should be able to collapse the Elements container', () => {
+    cy.visit(`${Cypress.config('baseUrl')}/elements`);
+
+    cy.contains('.element-group', 'Elements').within(() => {
+      cy.get('.element-list').should('be.visible');
+
+      cy.get('.header-right').click();
+
+      cy.get('.element-list').should('not.have.class', 'show');
     });
+  });
 
-    specify('As a user, I should be able to collapse the Elements container', () => {
-        cy.visit(`${Cypress.config('baseUrl')}/elements`);
+  const buttonTuple = [
+    ['Double Click', 'doubleClickMessage'],
+    ['Right Click', 'rightClickMessage'],
+    ['Click', 'dynamicClickMessage'],
+  ];
 
-        cy.contains('.element-group', 'Elements')
-          .within(() => {
-              cy.get('.element-list')
-                .should('be.visible');
+  buttonTuple.forEach(($type) => {
+    const [clickType, messageSelector] = $type;
 
-              cy.get('.header-right')
-                .click();
+    specify(`As a user, I should be able to ${clickType} an element`, () => {
+      cy.visit(`${Cypress.config('baseUrl')}/buttons`);
 
-              cy.get('.element-list')
-                .should('not.have.class', 'show');
-          });
+      if (clickType === 'Double Click') {
+        cy.get('#doubleClickBtn').dblclick();
+      } else if (clickType === 'Right Click') {
+        cy.get('#rightClickBtn').rightclick();
+      } else {
+        cy.get('button').last().click();
+      }
+
+      cy.get(`#${messageSelector}`).should('be.visible');
     });
-
-    const buttonTuple = [
-        ['Double Click', 'doubleClickMessage'],
-        ['Right Click', 'rightClickMessage'],
-        ['Click', 'dynamicClickMessage']
-    ];
-
-    buttonTuple.forEach(($type) => {
-        const [clickType, messageSelector] = $type;
-
-        specify(`As a user, I should be able to ${clickType} an element`, () => {
-            cy.visit(`${Cypress.config('baseUrl')}/buttons`);
-
-            if (clickType === 'Double Click') {
-                cy.get('#doubleClickBtn')
-                  .dblclick();
-            } else if (clickType === 'Right Click') {
-                cy.get('#rightClickBtn')
-                  .rightclick();
-            } else {
-                cy.get('button')
-                  .last()
-                  .click();
-            }
-
-            cy.get(`#${messageSelector}`)
-              .should('be.visible');
-        });
-    });
+  });
 });
