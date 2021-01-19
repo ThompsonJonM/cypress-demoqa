07d55882d5e9d16bdb8ff1cf17eac5f26e5d5255
thompsonjonm <thompson.jonm@gmail.com>
2021-01-18 19:58:53 -0500

    Add and delete book tests

--- cypress/integration/bookstore.spec.js
+++ cypress/integration/bookstore.spec.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 describe('DemoQA Book Store App', () => {
+  const book = {
+    title: 'Designing Evolvable Web APIs with ASP.NET',
+    ISBN: '9781449337711',
+  };
   const user = {
     username: Cypress.env('bookstoreUser').username,
     password: Cypress.env('bookstoreUser').password,
@@ -13,27 +17,64 @@ describe('DemoQA Book Store App', () => {
   });
 
   context('Adding Books', () => {
-    before('Authenticate', () => {
+    beforeEach('Authenticate', () => {
       cy.authenticate(user);
     });
 
-    after('Delete the book', () => {
-      cy.deleteBook('9781449337711');
+    afterEach('Delete the book', () => {
+      cy.deleteBook(book.ISBN);
     });
 
     specify('Add a book via API', () => {
-      cy.addBook('9781449337711');
+      cy.addBook(book.ISBN);
+    });
+
+    specify('As a user, I should be able to add a book to my profile', () => {
+      cy.visit(`${Cypress.config('baseUrl')}/books?book=${book.ISBN}`);
+
+      cy.url().should('contain', book.ISBN);
+      cy.get('.profile-wrapper').within(() => {
+        for (const [key, value] of Object.entries(book)) {
+          cy.get(`#${key}-wrapper`).should('contain', value);
+        }
+      });
+
+      cy.get('.text-right').within(() => {
+        cy.get('#addNewRecordButton').click();
+      });
+
+      cy.visit(`${Cypress.config('baseUrl')}/profile`);
+
+      cy.contains('.rt-tr-group', book.title).should('be.visible');
     });
   });
 
   context('Deleting Books', () => {
-    before('Add a book', () => {
+    beforeEach('Add a book', () => {
       cy.authenticate(user);
-      cy.addBook('9781449337711');
+      cy.addBook(book.ISBN);
     });
 
     specify('Delete a book via API', () => {
-      cy.deleteBook('9781449337711');
+      cy.deleteBook(book.ISBN);
+    });
+
+    specify('As a user, I should be able to delete a book from my profile', () => {
+      cy.visit(`${Cypress.config('baseUrl')}/profile`);
+
+      cy.contains('.rt-tr-group', book.title).within(() => {
+        cy.get('#delete-record-undefined').click();
+      });
+
+      cy.get('.modal-content').within(() => {
+        cy.get('#closeSmallModal-ok').click();
+      });
+
+      cy.get('.modal-content').should('not.exist');
+
+      cy.reload();
+
+      cy.contains('.rt-tr-group', book.title).should('not.exist');
     });
   });
 });
